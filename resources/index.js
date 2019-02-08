aadb464b1525418edbf3cd695f756041c1d32dde
maryplank <maryplank@users.noreply.github.com>
2019-02-08 12:44:01 -0200

    Update index.js

--- resources/index.js
+++ resources/index.js
@@ -1,4 +1,4 @@
-//Aliases
+// Aliases
 
 let Application = PIXI.Application,
   base = PIXI.BaseTexture,
@@ -15,7 +15,7 @@ let Application = PIXI.Application,
   texture = PIXI.Texture,
   TextureCache = PIXI.utils.TextureCache;
 
-//Create a Pixi Application
+// Create a Pixi Application
 let app = new Application({
   width: 592,
   height: 644,
@@ -24,33 +24,34 @@ let app = new Application({
   resolution: 1
 });
 
-//tink 
+/*tink (ended up not using!)
 let t = new Tink(PIXI, app.renderer.view),
   pointer = t.makePointer();
+*/
 
-//Add the canvas that Pixi automatically created for you to the HTML document
+// PIXI canvas
 document.body.appendChild(app.view);
 
-//loader
+// loader
 loader
   .add("resources/images/escape.json")
   .on("progress", loadProgressHandler)
   .load(setup);
 
-//variables for functions
-let state, windows, boy, boyFace, bed, blocksGame, blocksInventory, chest, box, exit, keyInventory, keyGame, player, room, table, tv,
-  door, message, gameScene, gameOverScene, enemies, id, clickX, clickY;
+// global variables
+let state, windows, bed, blocksGame, blocksInventory, boy, boyFace, box, chest, clickX, clickY, door, exit, gameScene, gameOverScene, 
+id, key, message, player, room, table, tv;
 
 function setup() {
-  //game scene
+  // game scene
   gameScene = new Container();
   gameScene.visible = true;
   app.stage.addChild(gameScene);
 
-  //alias for sprites id
+  // alias for sprites id
   id = resources["resources/images/escape.json"].textures;
 
-  //room
+  // room
   room = new Sprite(id["room.png"]);
   room.position.set(80, 110);
   room.width = room.width / 5;
@@ -59,7 +60,7 @@ function setup() {
   room.on('mousedown', moveme);
   gameScene.addChild(room);
 
-  //door
+  // door
   door = new Sprite(id["door.png"]);
   door.width = door.width / 5;
   door.height = door.height / 5;
@@ -67,7 +68,7 @@ function setup() {
   door.y = 146;
   gameScene.addChild(door);
 
-  //bed
+  // bed
   bed = new Sprite(id["bed.png"]);
   bed.width = bed.width / 5;
   bed.height = bed.height / 5;
@@ -75,7 +76,7 @@ function setup() {
   bed.y = 215;
   gameScene.addChild(bed);
 
-  //table
+  // table
   table = new Sprite(id["table.png"]);
   table.width = table.width / 5;
   table.height = table.height / 5;
@@ -83,63 +84,22 @@ function setup() {
   table.y = (room.y + room.height) - (table.height + 32);
   gameScene.addChild(table);
 
-  //tv
-  tv = new Sprite(id["tv.png"]);
-  tv.width = tv.width / 5;
-  tv.height = tv.height / 5;
-  tv.x = (room.x + room.width) / 2;
-  tv.y = (room.y + room.height) - (tv.height + 32)
-  gameScene.addChild(tv);
-
-  //key
-  keyGame = new Sprite(id["key.png"]);
-  keyGame.visible = false;
-  keyGame.width = keyGame.width / 5;
-  keyGame.height = keyGame.height / 5;
-  gameScene.addChild(keyGame);
-  t.makeInteractive(keyGame);
-  keyGame.press = () => {
-    if (blocksGame.visible && keyInventory.visible) {
-      keyGame.visible = true;
-      keyGame.x = boy.x - 10;
-      keyGame.y = boy.y;
-    };
-  }
-
-  keyInventory = new Sprite(id["key.png"]);
-  keyInventory.antialias = false;
-  keyInventory.position.set(30, 146);
-  keyInventory.width = 26;
-  keyInventory.height = 48;
-  keyInventory.visible = false;
-  gameScene.addChild(keyInventory);
-
-  //blocks
+  // blocks
   blocksGame = new Sprite(id["blocks.png"]);
   blocksGame.width = blocksGame.width / 5;
   blocksGame.height = blocksGame.height / 5;
   blocksGame.position.set(203, 206);
   blocksGame.visible = false;
   gameScene.addChild(blocksGame);
-  t.makeInteractive(blocksGame);
-  blocksGame.press = () => {
-    if (blocksInventory.visible) {
-      blocksGame.visible = true;
-      boy.position.set(203, 220);
-      messageGame.text = "I can see above the window from\nhere!";
-    } else {
-      messageGame.text = "I don't know what to do here."
-    };
-  };
 
   blocksInventory = new Sprite(id["blocks.png"]);
-  blocksInventory.position.set(22, 222);
+  blocksInventory.position.set(22, 160);
   blocksInventory.width = 42;
   blocksInventory.height = 42;
   blocksInventory.visible = false;
   gameScene.addChild(blocksInventory);
 
-  //window
+  // window
   windows = new Sprite(id["windows.png"]);
   windows.width = windows.width / 5;
   windows.height = windows.height / 5;
@@ -147,27 +107,43 @@ function setup() {
   windows.y = (blocksGame.y - windows.height) + 2;
   gameScene.addChild(windows);
 
-  //chest
+  // chest
   chest = new Sprite(id["chest.png"]);
   chest.width = chest.width / 5;
   chest.height = chest.height / 5;
   chest.x = room.x + 16;
   chest.y = 350;
   gameScene.addChild(chest);
-  t.makeInteractive(chest);
 
-  //boy
+  // boy
   boy = new Sprite(id["boy.png"]);
-  boy.position.set(260, 260);
+  boy.position.set(360, 260);
   boy.width = boy.width / 5;
   boy.height = boy.height / 5;
   boy.vx = 0;
   boy.vy = 0;
   gameScene.addChild(boy);
 
-  //dialog bar
+  // key
+  key = new Sprite(id["key.png"]);
+  key.visible = false;
+  key.width = key.width / 5;
+  key.height = key.height / 5;
+  key.x = 35;
+  key.y = 222;
+  gameScene.addChild(key);
 
-  //character box
+  // tv
+  tv = new Sprite(id["tv.png"]);
+  tv.width = tv.width / 5;
+  tv.height = tv.height / 5;
+  tv.x = (room.x + room.width) / 2;
+  tv.y = (room.y + room.height) - (tv.height + 32)
+  gameScene.addChild(tv);
+
+  // dialog bar
+
+  // character box
   box = new Graphics();
   box.beginFill(0x626262);
   box.lineStyle(3, 0xffffff, 1);
@@ -181,7 +157,7 @@ function setup() {
   boyFace.height = boyFace.height / 5;
   gameScene.addChild(boyFace);
 
-  //character texts
+  // character texts
   let charStyle = new TextStyle({
     fontFamily: "Courier New",
     fontWeight: "bold",
@@ -189,44 +165,44 @@ function setup() {
     fill: "white"
   });
   messageGame = new Text("I need to get out of the room!", charStyle);
-  messageGame.position.set(194, 56);
+  messageGame.position.set(194, 44);
   messageGame.wordWrap = true;
   gameScene.addChild(messageGame);
 
-  //game over scene
+  // game over scene
   gameOverScene = new Container();
   app.stage.addChild(gameOverScene);
   gameOverScene.visible = false;
 
-  //game over texts
+  // game over texts
   let style = new TextStyle({
-    fontFamily: "Futura",
+    fontFamily: "Courier New",
     fontWeight: "bold",
     fontSize: 64,
     fill: "white"
   });
   message = new Text("The End!", style);
   message.x = 120,
-    message.y = app.stage.height / 2 - 32;
+  message.y = app.stage.height / 2 - 32;
   gameOverScene.addChild(message);
 
   state = play;
   app.ticker.add(delta => gameLoop(delta));
 };
 
-//function game loop
+// function game loop
 function gameLoop(delta) {
   state(delta);
-  t.update();
+  //t.update();
 };
-let hitChest = false;
-//function play
+
+// function play
 function play(delta) {
   boy.x += boy.vx;
   boy.y += boy.vy;
   contain(boy, { x: 96, y: 215, width: 576, height: 590 });
   
-  //move boy
+  // move boy
   if (boy.x > clickX) {
     boy.vx = -1;
   } else if (boy.x < clickX) {
@@ -242,17 +218,62 @@ function play(delta) {
   } else {
     boy.vy = 0;
   }
-  
+
+  // interaction boy - chest
   if (hitTestRectangle(boy, chest)) {
     boy.vx = 0;
     boy.vy = 0;
     boy.x = boy.x + 1;
     boy.y = boy.y + 1;
-    hitChest = true;
 
     blocksInventory.visible = true;
     messageGame.position.set(194, 26);
     messageGame.text = "I found my blocks!\nIf I build them high enough\nI can climb them.";
+  }
+
+  // interaction boy - blocks
+  if (hitTestRectangle(boy, blocksGame) && blocksInventory.visible) {
+    blocksGame.visible = true;
+    blocksInventory.visible = false;
+
+  } else if (hitTestRectangle(boy, blocksGame) && blocksGame.visible) {
+    messageGame.position.set(194, 26);
+    messageGame.text = "I wonder if I can see\nabove the window...\nI found a key!";
+    key.visible = true;
+    // insert animation of boy above the window...
+
+  } else if (hitTestRectangle(boy, blocksGame) && !blocksGame.visible){
+    messageGame.text = "I don't know what to do here.";
+  }
+
+  // interaction boy - table
+  if (hitTestRectangle(boy, table)) {
+    boy.vx = 0;
+    boy.vy = 0;
+    boy.x = boy.x - 1;
+    boy.y = boy.y - 1;
+    messageGame.position.set(194, 34);
+    messageGame.text = "There's a note on the table\nbut I'm five! I can't read!";
+   }
+
+  // interaction boy - door
+  if (hitTestRectangle(boy, door) && !key.visible) {
+    messageGame.position.set(194, 26);
+    messageGame.text = "The door is locked.\nMommy must have left\na key here somewhere...";
+  } else if (hitTestRectangle(boy, door) && key.visible) {
+    state = end;
+  }
+
+  // interaction boy - bed
+  if (hitTestRectangle(boy, bed)) {
+    boy.vx = 0;
+    boy.vy = 0;
+    boy.y = boy.y + 1;
+    if (boy.x > bed.x) { 
+      boy.x + 1; 
+    } else if (boy.x < bed.x + bed.length) {
+      boy.x - 1;
+    }
   }
 };
 
@@ -261,143 +282,3 @@ function end() {
   gameScene.visible = false;
   gameOverScene.visible = true;
 }
-
-// HELPER FUNCTIONS 
-
-//function move me
-function moveme(xx) {
-  clickX = xx.data.global.x;
-  clickY = xx.data.global.y;
-}
-
-//function contain
-function contain(sprite, container) {
-
-  let collision = undefined;
-
-  //Left
-  if (sprite.x < container.x) {
-    sprite.x = container.x;
-    collision = "left";
-  }
-
-  //Top
-  if (sprite.y < container.y) {
-    sprite.y = container.y;
-    collision = "top";
-  }
-
-  //Right
-  if (sprite.x + sprite.width > container.width) {
-    sprite.x = container.width - sprite.width;
-    collision = "right";
-  }
-
-  //Bottom
-  if (sprite.y + sprite.height > container.height) {
-    sprite.y = container.height - sprite.height;
-    collision = "bottom";
-  }
-
-  //Return the `collision` value
-  return collision;
-};
-
-//random number inside stage
-function randomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-//loader function. Logs the loaded sprites
-function loadProgressHandler(loader, resource) {
-  console.log("loading: " + resource.url);
-  console.log("progress: " + loader.progress + "%");
-};
-
-//function hit
-function hitTestRectangle(r1, r2) {
-  //Define the variables we'll need to calculate
-  let hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
-  //hit will determine whether there's a collision
-  hit = false;
-  //Find the center points of each sprite
-  r1.centerX = r1.x + r1.width / 2;
-  r1.centerY = r1.y + r1.height / 2;
-  r2.centerX = r2.x + r2.width / 2;
-  r2.centerY = r2.y + r2.height / 2;
-  //Find the half-widths and half-heights of each sprite
-  r1.halfWidth = r1.width / 2;
-  r1.halfHeight = r1.height / 2;
-  r2.halfWidth = r2.width / 2;
-  r2.halfHeight = r2.height / 2;
-  //Calculate the distance vector between the sprites
-  vx = r1.centerX - r2.centerX;
-  vy = r1.centerY - r2.centerY;
-  //Figure out the combined half-widths and half-heights
-  combinedHalfWidths = r1.halfWidth + r2.halfWidth;
-  combinedHalfHeights = r1.halfHeight + r2.halfHeight;
-  //Check for a collision on the x axis
-  if (Math.abs(vx) < combinedHalfWidths) {
-    //A collision might be occuring. Check for a collision on the y axis
-    if (Math.abs(vy) < combinedHalfHeights) {
-      //There's definitely a collision happening
-      hit = true;
-    } else {
-      //There's no collision on the y axis
-      hit = false;
-    }
-  } else {
-    //There's no collision on the x axis
-    hit = false;
-  }
-  //`hit` will be either `true` or `false`
-  return hit;
-};
-
-//funcion Keyboard
-function keyboard(value) {
-  let keyInventory = {};
-  keyInventory.value = value;
-  keyInventory.isDown = false;
-  keyInventory.isUp = true;
-  keyInventory.press = undefined;
-  keyInventory.release = undefined;
-  //The `downHandler`
-  keyInventory.downHandler = event => {
-    if (event.keyInventory === keyInventory.value) {
-      if (keyInventory.isUp && keyInventory.press) keyInventory.press();
-      keyInventory.isDown = true;
-      keyInventory.isUp = false;
-      event.preventDefault();
-    }
-  };
-
-  //The `upHandler`
-  keyInventory.upHandler = event => {
-    if (event.keyInventory === keyInventory.value) {
-      if (keyInventory.isDown && keyInventory.release) keyInventory.release();
-      keyInventory.isDown = false;
-      keyInventory.isUp = true;
-      event.preventDefault();
-    }
-  };
-
-  //Attach event listeners
-  const downListener = keyInventory.downHandler.bind(keyInventory);
-  const upListener = keyInventory.upHandler.bind(keyInventory);
-
-  window.addEventListener(
-    "keydown", downListener, false
-  );
-  window.addEventListener(
-    "keyup", upListener, false
-  );
-
-  // Detach event listeners
-  keyInventory.unsubscribe = () => {
-    window.removeEventListener("keydown", downListener);
-    window.removeEventListener("keyup", upListener);
-  };
-
-  return keyInventory;
-};
