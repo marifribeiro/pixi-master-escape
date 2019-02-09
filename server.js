e015d26ed3ce02db804b7a6bd873d4951c8524e9
maryplank <maryplank@users.noreply.github.com>
2019-02-09 18:48:55 -0200

    Fix port

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
+const connect = require('connect');
+const serveStatic = require('serve-static');
+const port = process.env.PORT || 8080;
+
+connect().use(serveStatic(__dirname)).listen(port, function(){
+    console.log('Server running');
 });
