cee071e27726b42f5ce9252522ad993174272243
Mariana Ribeiro <mariana-fribeiro@live.com>
2019-11-24 13:22:17 -0300

    updated server message

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const serveStatic = require('serve-static');
 const port = process.env.PORT || 8080;
 
 connect().use(serveStatic(__dirname)).listen(port, function(){
-    console.log('Server running');
+    console.log('Server running, access: http://localhost:8080/index.html');
 });
